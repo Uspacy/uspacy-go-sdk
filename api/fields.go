60878d56606585ae641a892dd0294955786a0ffd
GoopaloUA <b.nepochatov@alterego.digital>
2023-03-13 14:26:00 +0200

    entityType fix

--- api/fields.go
+++ api/fields.go
@@ -8,8 +8,8 @@ import (
 )
 
 // GetFields returns Fields struct for a given type of entity
-func (us *Uspacy) GetFields(entity string) (fields crm.Fields, err error) {
-	body, err := us.doGetEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.FieldsUrl, entity, "")))
+func (us *Uspacy) GetFields(entityType crm.Entity) (fields crm.Fields, err error) {
+	body, err := us.doGetEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.FieldsUrl, entityType.GetUrl(crm.EntityType), "")))
 	if err != nil {
 		return fields, err
 	}
@@ -17,17 +17,17 @@ func (us *Uspacy) GetFields(entity string) (fields crm.Fields, err error) {
 }
 
 // GetField returns Field struct for a given type of entity & field
-func (us *Uspacy) GetField(entity, fieldType string) (field crm.Field, err error) {
-	body, err := us.doGetEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.FieldsUrl, entity, fieldType)))
+func (us *Uspacy) GetField(entityType crm.Entity, fieldType string) (field crm.Field, err error) {
+	body, err := us.doGetEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.FieldsUrl, entityType.GetUrl(crm.EntityType), fieldType)))
 	if err != nil {
 		return field, err
 	}
 	return field, json.Unmarshal(body, &field)
 }
 
-// CreateField in CRM entity returns created field
-func (us *Uspacy) CreateField(entityType crm.Entity, field interface{}) (createdField crm.Field, err error) {
-	responseBody, err := us.doPostEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.EntityUrl, entityType.GetUrl(crm.StatusId))), field)
+// CreateCRMField in CRM entity returns created field
+func (us *Uspacy) CreateCRMField(entityType crm.Entity, field interface{}) (createdField crm.Field, err error) {
+	responseBody, err := us.doPostEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.FieldsUrl, entityType.GetUrl(crm.EntityType), "")), field)
 	if err != nil {
 		return createdField, err
 	}
--- api/kanban-stage.go
+++ api/kanban-stage.go
@@ -8,8 +8,8 @@ import (
 )
 
 // CreateKanbanStage returns created kanban stage
-func (us *Uspacy) CreateKanbanStage(entity string, body interface{}) (kanbanStage crm.KanbanStage, err error) {
-	responseBody, err := us.doPostEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.KanbanStageUrl, entity)), body)
+func (us *Uspacy) CreateKanbanStage(entityType crm.Entity, body interface{}) (kanbanStage crm.KanbanStage, err error) {
+	responseBody, err := us.doPostEmptyHeaders(buildURL(mainHost, crm.VersionCRMUrl, fmt.Sprintf(crm.KanbanStageUrl, entityType.GetUrl(crm.EntityType))), body)
 	if err != nil {
 		return kanbanStage, err
 	}
