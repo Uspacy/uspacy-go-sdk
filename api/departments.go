88a40133708a66afb1f9b166e582e2a33e172011
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-04-13 10:32:44 +0300

    Feature/add departments (#26)
    * add departments
    
    * add departments Patch

---
+++ api/departments.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Uspacy/uspacy-go-sdk/departments"
+)
+
+// CreateActiveDepartment returns created department
+func (us *Uspacy) CreateActiveDepartment(departmentData departments.Department) (department []departments.Department, err error) {
+	body, err := us.doPostEmptyHeaders(us.buildURL(departments.VersionUrl, departments.DepartmentsUrl), departmentData)
+	if err != nil {
+		return department, err
+	}
+	return department, json.Unmarshal(body, &department)
+}
+
+// PatchDepartment patch department by Id and return it
+func (us *Uspacy) PatchDepartment(departmenID int, departmentData map[string]interface{}) (department departments.Department, err error) {
+	body, err := us.doPatchEmptyHeaders(us.buildURL(departments.VersionUrl, fmt.Sprintf(departments.DepartmentsUrl, departmenID)), departmentData)
+	if err != nil {
+		return department, err
+	}
+	return department, json.Unmarshal(body, &department)
+}
