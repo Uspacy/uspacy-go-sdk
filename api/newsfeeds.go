73a86cd8ef92228c342bdd24da0518cb0f84dab2
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-04-13 10:32:31 +0300

    Hotfiх/add posts in newsfeed (#27)
    * add CreateNewsfeedPost
    
    * add CreateNewsfeedPost

--- api/newsfeeds.go
+++ api/newsfeeds.go
@@ -7,6 +7,15 @@ import (
 	"github.com/Uspacy/uspacy-go-sdk/newsfeed"
 )
 
+// CreateNewsfeedPost returns created post
+func (us *Uspacy) CreateNewsfeedPost(postData newsfeed.Post) (post newsfeed.Post, err error) {
+	body, err := us.doPostEmptyHeaders(newsfeed.VersionUrl, postData)
+	if err != nil {
+		return post, err
+	}
+	return post, json.Unmarshal(body, &post)
+}
+
 // GetNewsfeeds gets all newsfeeds
 func (us *Uspacy) GetNewsfeeds(page, list, groupId int) (posts newsfeed.GetNewsfeed, err error) {
 	body, err := us.doGetEmptyHeaders(us.buildURL(newsfeed.VersionUrl, fmt.Sprintf(newsfeed.PostsUrl, page, list, groupId)))
