5161c35f73b32d07e3d70a9dc31d05aa7e4903c4
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-04-17 20:52:50 +0300

    CreateTask Add (#33)

--- api/crm.go
+++ api/crm.go
@@ -62,7 +62,7 @@ func (us *Uspacy) CreateDeal(entityType crm.Entity, dealData map[string]interfac
 }
 
 // CreateTask returns created task object
-func (us *Uspacy) CreateTask(entityType crm.Entity, taskData map[string]interface{}) (task crm.Task, err error) {
+func (us *Uspacy) CreateTaskCRM(entityType crm.Entity, taskData map[string]interface{}) (task crm.Task, err error) {
 	body, err := us.doPostEmptyHeaders(us.buildURL(crm.VersionUrl, "static/tasks"), taskData)
 	if err != nil {
 		return task, err
