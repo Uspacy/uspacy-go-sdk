6fd20a678a82ad0bcc28ea0228c431ce6a636dd5
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-04-17 21:28:33 +0300

    Add create group (#34)

--- api/groups.go
+++ api/groups.go
@@ -2,9 +2,19 @@ package api
 
 import (
 	"encoding/json"
+	"net/url"
 
 	"github.com/Uspacy/uspacy-go-sdk/group"
 )
+
+// CreateGroup returns created group object
+func (us *Uspacy) CreateGroup(groupData url.Values) (_group group.Group, err error) {
+	body, err := us.doPostFormData(us.buildURL(group.VersionUrl, group.GroupUrl), groupData)
+	if err != nil {
+		return _group, err
+	}
+	return _group, json.Unmarshal(body, &_group)
+}
 
 // CreateTransferGroup creates a new transfer group
 func (us *Uspacy) CreateTransferGroup(body interface{}) (groups group.TransferGroupOutput, err error) {
