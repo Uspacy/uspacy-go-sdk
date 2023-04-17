f7d74f7ae3f6bf8d583ead2099644e332c08d387
GoopaloUA <b.nepochatov@alterego.digital>
2023-04-17 19:43:15 +0300

    CreateTask Add

--- api/tasks.go
+++ api/tasks.go
@@ -6,6 +6,15 @@ import (
 	"github.com/Uspacy/uspacy-go-sdk/task"
 )
 
+// CreateTask creates a new task
+func (us *Uspacy) CreateTask(taskData task.Task) (_task task.Task, err error) {
+	resp, err := us.doPostEmptyHeaders(us.buildURL(task.VersionUrl, task.TaskUrl), taskData)
+	if err != nil {
+		return _task, err
+	}
+	return _task, json.Unmarshal(resp, &_task)
+}
+
 // CreateTransferTask creates a new transfer task
 func (us *Uspacy) CreateTransferTask(body interface{}) (tasks task.TransferTaskOutput, err error) {
 	resp, err := us.doPostEmptyHeaders(us.buildURL(task.VersionUrl, task.TransferUrl), body)
