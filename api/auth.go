e9f7b3002cfb644c6708254945d865e35f89830e
GoopaloUA <b.nepochatov@alterego.digital>
2023-04-17 12:24:46 +0300

    refresh token by Domain

--- api/auth.go
+++ api/auth.go
@@ -1,20 +1,27 @@
 package api
 
 import (
+	"encoding/base64"
 	"encoding/json"
-	"github.com/Uspacy/uspacy-go-sdk/auth"
+	"fmt"
 	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Uspacy/uspacy-go-sdk/auth"
 )
 
 func (us *Uspacy) getToken() string {
 	return us.bearerToken
-
 }
 
 func (us *Uspacy) refreshToken() {
 	var refresh auth.RefreshOutput
-	authClient := New(us.bearerToken, auth.AuthHost)
-	body, err := authClient.doPostEmptyHeaders(us.buildURL(auth.VersionUrl, auth.RefreshTokenUrl), nil)
+	body, err := us.doRaw(
+		fmt.Sprintf("%s%s/%s/%s", "https://", us.UnmarshalTokenData().Domain, auth.VersionUrl, auth.RefreshTokenUrl),
+		http.MethodPost,
+		emptyHeaders,
+		nil)
 	if err != nil {
 		log.Fatal("error while trying to refresh token: ", err)
 	}
@@ -23,5 +30,13 @@ func (us *Uspacy) refreshToken() {
 		log.Fatal("error while trying to parse token: ", err)
 	}
 	us.bearerToken = refresh.Jwt
+}
 
+func (us *Uspacy) UnmarshalTokenData() (tokenData auth.TokenData) {
+	strings := strings.Split(strings.Join(strings.Split(us.bearerToken, " "), "."), ".")
+	for _, _string := range strings {
+		decoded, _ := base64.RawURLEncoding.DecodeString(_string)
+		json.Unmarshal(decoded, &tokenData)
+	}
+	return
 }
--- auth/types.go
+++ auth/types.go
@@ -2,37 +2,45 @@ package auth
 
 const (
 	VersionUrl      = "auth/v1"
-	AuthHost        = "http://auth-service"
 	RefreshTokenUrl = "auth/refresh_token"
 )
 
-type RefreshOutput struct {
-	Jwt             string `json:"jwt"`
-	RefreshToken    string `json:"refreshToken"`
-	ExpireInSeconds int64  `json:"expireInSeconds"`
-}
+type (
+	RefreshOutput struct {
+		Jwt             string `json:"jwt"`
+		RefreshToken    string `json:"refreshToken"`
+		ExpireInSeconds int64  `json:"expireInSeconds"`
+	}
 
-type JwtClaims struct {
-	Iss         string   `json:"iss"`
-	Iat         int64    `json:"iat"`
-	Exp         int64    `json:"exp"`
-	Nbf         int64    `json:"nbf"`
-	Jti         string   `json:"jti"`
-	Id          int64    `json:"id"`
-	Email       string   `json:"email"`
-	Sub         string   `json:"sub"`
-	AuthUserId  int      `json:"authUserId"`
-	FirstName   string   `json:"firstName"`
-	LastName    string   `json:"lastName"`
-	WorkspaceId string   `json:"workspaceId"`
-	Domain      string   `json:"domain"`
-	Roles       []string `json:"roles"`
-	Departments []int    `json:"departments"`
-	Permissions struct {
-		Create []string `json:"create"`
-		View   []string `json:"view"`
-		Edit   []string `json:"edit"`
-		Delete []string `json:"delete"`
-	} `json:"permissions"`
-	TariffId int `json:"tariffId"`
-}
+	TokenData struct {
+		Domain      string `json:"domain"`
+		Id          int    `json:"id"`
+		Sub         string `json:"sub"`
+		WorkspaceID string `json:"workspaceId"`
+	}
+
+	JwtClaims struct {
+		Iss         string   `json:"iss"`
+		Iat         int64    `json:"iat"`
+		Exp         int64    `json:"exp"`
+		Nbf         int64    `json:"nbf"`
+		Jti         string   `json:"jti"`
+		Id          int64    `json:"id"`
+		Email       string   `json:"email"`
+		Sub         string   `json:"sub"`
+		AuthUserId  int      `json:"authUserId"`
+		FirstName   string   `json:"firstName"`
+		LastName    string   `json:"lastName"`
+		WorkspaceId string   `json:"workspaceId"`
+		Domain      string   `json:"domain"`
+		Roles       []string `json:"roles"`
+		Departments []int    `json:"departments"`
+		Permissions struct {
+			Create []string `json:"create"`
+			View   []string `json:"view"`
+			Edit   []string `json:"edit"`
+			Delete []string `json:"delete"`
+		} `json:"permissions"`
+		TariffId int `json:"tariffId"`
+	}
+)
