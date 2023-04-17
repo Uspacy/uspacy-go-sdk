70966df104acdd951a967e9fc4664b7744dfb6b4
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-04-17 18:42:43 +0300

    Add comments service (#31)

---
+++ api/comments.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"encoding/json"
+
+	"github.com/Uspacy/uspacy-go-sdk/comments"
+)
+
+// CreateComment returns created comment
+func (us *Uspacy) CreateComment(commentsData comments.Comment) (comment comments.Comment, err error) {
+	body, err := us.doPostEmptyHeaders(us.buildURL(comments.VersionUrl, comments.CommentsUrl), commentsData)
+	if err != nil {
+		return comment, err
+	}
+	return comment, json.Unmarshal(body, &comment)
+}
---
+++ comments/types.go
@@ -0,0 +1,15 @@
+package comments
+
+const (
+	VersionUrl  = "comments/v1"
+	CommentsUrl = "comments/%v"
+)
+
+type Comment struct {
+	ID         int    `json:"id"`
+	AuthorID   int    `json:"authorId"`
+	EntityType string `json:"entityType"`
+	EntityID   int    `json:"entityId"`
+	Message    string `json:"message"`
+	Date       int    `json:"date"`
+}
