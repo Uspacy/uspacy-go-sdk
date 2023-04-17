2a1d78f7d923b32ff276d2f4a129d9e2dbe742a6
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-04-17 13:37:51 +0300

    Hotfix/refresh token fix (#28)
    * Update types.go
    
    * Update auth.go
    
    * Update types.go
    
    * refresh token by Domain
    
    * refresh token add fields
    
    * refresh token fix struct
    
    * return err while refresh token

--- api/auth.go
+++ api/auth.go
@@ -1,27 +1,46 @@
 package api
 
 import (
+	"encoding/base64"
 	"encoding/json"
-	"github.com/Uspacy/uspacy-go-sdk/auth"
+	"fmt"
 	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Uspacy/uspacy-go-sdk/auth"
 )
 
-func (us *Uspacy) getToken() string {
-	return us.bearerToken
-
-}
-
-func (us *Uspacy) refreshToken() {
+func (us *Uspacy) refreshToken() error {
 	var refresh auth.RefreshOutput
-
-	body, err := us.doPostEmptyHeaders(auth.VersionUrl+auth.RefreshTokenUrl, nil)
+	jwt, err := us.UnmarshalTokenData()
+	if err != nil {
+		log.Fatal("error while trying to unmarshal token: ", err)
+		return err
+	}
+	body, err := us.doRaw(
+		fmt.Sprintf("%s%s/%s/%s", "https://", jwt.Domain, auth.VersionUrl, auth.RefreshTokenUrl),
+		http.MethodPost,
+		emptyHeaders,
+		nil)
 	if err != nil {
 		log.Fatal("error while trying to refresh token: ", err)
+		return err
 	}
 	err = json.Unmarshal(body, &refresh)
 	if err != nil {
 		log.Fatal("error while trying to parse token: ", err)
+		return err
 	}
 	us.bearerToken = refresh.Jwt
+	return nil
+}
 
+func (us *Uspacy) UnmarshalTokenData() (tokenData auth.JwtClaims, err error) {
+	strings := strings.Split(strings.Join(strings.Split(us.bearerToken, " "), "."), ".")
+	for _, _string := range strings {
+		decoded, _ := base64.RawURLEncoding.DecodeString(_string)
+		err = json.Unmarshal(decoded, &tokenData)
+	}
+	return
 }
--- auth/types.go
+++ auth/types.go
@@ -1,38 +1,39 @@
 package auth
 
-const VersionUrl = "auth/v1/"
-
 const (
+	VersionUrl      = "auth/v1"
 	RefreshTokenUrl = "auth/refresh_token"
 )
 
-type RefreshOutput struct {
-	Jwt             string `json:"jwt"`
-	RefreshToken    string `json:"refreshToken"`
-	ExpireInSeconds int64  `json:"expireInSeconds"`
-}
+type (
+	RefreshOutput struct {
+		Jwt             string `json:"jwt"`
+		RefreshToken    string `json:"refreshToken"`
+		ExpireInSeconds int64  `json:"expireInSeconds"`
+	}
 
-type JwtClaims struct {
-	Iss         string   `json:"iss"`
-	Iat         int64    `json:"iat"`
-	Exp         int64    `json:"exp"`
-	Nbf         int64    `json:"nbf"`
-	Jti         string   `json:"jti"`
-	Id          int64    `json:"id"`
-	Email       string   `json:"email"`
-	Sub         string   `json:"sub"`
-	AuthUserId  int      `json:"authUserId"`
-	FirstName   string   `json:"firstName"`
-	LastName    string   `json:"lastName"`
-	WorkspaceId string   `json:"workspaceId"`
-	Domain      string   `json:"domain"`
-	Roles       []string `json:"roles"`
-	Departments []int    `json:"departments"`
-	Permissions struct {
-		Create []string `json:"create"`
-		View   []string `json:"view"`
-		Edit   []string `json:"edit"`
-		Delete []string `json:"delete"`
-	} `json:"permissions"`
-	TariffId int `json:"tariffId"`
-}
+	JwtClaims struct {
+		Iss         string   `json:"iss"`
+		Iat         int64    `json:"iat"`
+		Exp         int64    `json:"exp"`
+		Nbf         int64    `json:"nbf"`
+		Jti         string   `json:"jti"`
+		Id          int64    `json:"id"`
+		Email       string   `json:"email"`
+		Sub         string   `json:"sub"`
+		AuthUserId  int      `json:"authUserId"`
+		FirstName   string   `json:"firstName"`
+		LastName    string   `json:"lastName"`
+		WorkspaceId string   `json:"workspaceId"`
+		Domain      string   `json:"domain"`
+		Roles       []string `json:"roles"`
+		Departments []int    `json:"departments"`
+		Permissions struct {
+			Create []string `json:"create"`
+			View   []string `json:"view"`
+			Edit   []string `json:"edit"`
+			Delete []string `json:"delete"`
+		} `json:"permissions"`
+		TariffId int `json:"tariffId"`
+	}
+)
