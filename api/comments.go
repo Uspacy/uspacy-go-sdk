82aa36fbb0cc50d8cf5d6b3a79defc3bd19a279c
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-04-18 18:30:07 +0300

    Hotfix/fixes for import (#40)
    * fix urls
    
    * fix buildURL

--- api/comments.go
+++ api/comments.go
@@ -2,13 +2,14 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/Uspacy/uspacy-go-sdk/comments"
 )
 
 // CreateComment returns created comment
 func (us *Uspacy) CreateComment(commentsData comments.Comment) (comment comments.Comment, err error) {
-	body, err := us.doPostEmptyHeaders(us.buildURL(comments.VersionUrl, comments.CommentsUrl), commentsData)
+	body, err := us.doPostEmptyHeaders(us.buildURL(comments.VersionUrl, fmt.Sprintf(comments.CommentsUrl, "")), commentsData)
 	if err != nil {
 		return comment, err
 	}
