ac69f4e597c788f0044c7589d58b5465b6060d0c
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-05-19 21:54:41 +0300

    hotfix (#67)

--- api/crm.go
+++ api/crm.go
@@ -63,7 +63,7 @@ func (us *Uspacy) CreateDeal(entityType crm.Entity, dealData map[string]interfac
 
 // CreateTask returns created task object
 func (us *Uspacy) CreateTaskCRM(entityType crm.Entity, taskData map[string]interface{}) (task crm.Task, err error) {
-	body, err := us.doPostEmptyHeaders(us.buildURL(crm.VersionUrl, crm.TaskUrl), taskData)
+	body, err := us.doPostEmptyHeaders(us.buildURL(crm.VersionUrl, fmt.Sprintf(crm.TaskUrl, "")), taskData)
 	if err != nil {
 		return task, err
 	}
