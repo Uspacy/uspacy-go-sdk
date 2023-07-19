5b922ad1f995d3ac216df834a0a2807f9d986827
GoopaloUA <b.nepochatov@alterego.digital>
2023-07-19 11:17:29 +0300

    new task through a map

--- api/tasks.go
+++ api/tasks.go
@@ -16,6 +16,15 @@ func (us *Uspacy) CreateTask(taskData url.Values) (_task task.Task, err error) {
 	return _task, json.Unmarshal(resp, &_task)
 }
 
+// CreateTask creates a new task through a map
+func (us *Uspacy) CreateTaskThroughMap(taskData map[string]interface{}) (_task task.Task, err error) {
+	resp, err := us.doPostEmptyHeaders(us.buildURL(task.VersionUrl, task.TaskUrl), taskData)
+	if err != nil {
+		return _task, err
+	}
+	return _task, json.Unmarshal(resp, &_task)
+}
+
 // CreateTransferTask creates a new transfer task
 func (us *Uspacy) CreateTransferTask(body interface{}) (tasks task.TransferTaskOutput, err error) {
 	resp, err := us.doPostEmptyHeaders(us.buildURL(task.VersionUrl, task.TransferUrl), body)
