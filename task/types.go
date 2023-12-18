9cef5fc9dbf5f5075d4650dbd613c51625dd3023
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2023-12-18 23:10:40 +0200

    Update Task struct in types.go (#90)

--- task/types.go
+++ task/types.go
@@ -9,26 +9,26 @@ const (
 
 type (
 	Task struct {
-		ID              string   `json:"id"`
-		ParentID        string   `json:"parentId"`
-		Title           string   `json:"title"`
-		Body            string   `json:"body"`
-		CreatedBy       string   `json:"createdBy"`
-		CreatedDate     int      `json:"createdDate"`
-		ClosedDate      int      `json:"closedDate"`
-		Deadline        int      `json:"deadline"`
-		DepartmentID    string   `json:"departmentId"`
-		GroupID         string   `json:"groupId"`
-		ClosedBy        string   `json:"closedBy"`
-		ResponsibleID   string   `json:"responsibleId"`
-		Status          string   `json:"status"`
-		Priority        string   `json:"priority"`
-		ResultCommentID string   `json:"resultCommentId"`
-		RequiredResult  bool     `json:"requiredResult"`
-		AcceptResult    bool     `json:"acceptResult"`
-		AccomplicesIds  []string `json:"accomplicesIds"`
-		AuditorsIds     []string `json:"auditorsIds"`
-		KanbanStageID   string   `json:"kanbanStageId"`
+		ID              string      `json:"id"`
+		ParentID        string      `json:"parentId"`
+		Title           string      `json:"title"`
+		Body            string      `json:"body"`
+		CreatedBy       string      `json:"createdBy"`
+		CreatedDate     int         `json:"createdDate"`
+		ClosedDate      int         `json:"closedDate"`
+		Deadline        int         `json:"deadline"`
+		DepartmentID    string      `json:"departmentId"`
+		GroupID         string      `json:"groupId"`
+		ClosedBy        string      `json:"closedBy"`
+		ResponsibleID   string      `json:"responsibleId"`
+		Status          string      `json:"status"`
+		Priority        string      `json:"priority"`
+		ResultCommentID string      `json:"resultCommentId"`
+		RequiredResult  bool        `json:"requiredResult"`
+		AcceptResult    bool        `json:"acceptResult"`
+		AccomplicesIds  []string    `json:"accomplicesIds"`
+		AuditorsIds     []string    `json:"auditorsIds"`
+		KanbanStageID   interface{} `json:"kanbanStageId"`
 	}
 
 	TransferTaskOutput struct {
