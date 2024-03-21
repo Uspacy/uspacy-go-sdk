7b1d8cce6f69f7e9c6f96340559ff78ab648e322
Nepochatov Bohdan <106430996+GoopaloUA@users.noreply.github.com>
2024-03-21 22:55:33 +0200

    add function of deleting files from fileservice (#94)
    * add function of deleting files from fileservice
    
    * add function of deleting files from fileservice by entityId

--- api/files.go
+++ api/files.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"encoding/json"
-	"github.com/Uspacy/uspacy-go-sdk/files"
+	"fmt"
 	"io"
+
+	"github.com/Uspacy/uspacy-go-sdk/files"
 )
 
 func (us *Uspacy) CreateFile(entityType, entityId string, filesMap map[string]io.ReadCloser) (file files.Files, err error) {
@@ -17,4 +19,22 @@ func (us *Uspacy) CreateFile(entityType, entityId string, filesMap map[string]io
 	}
 	return file, json.Unmarshal(body, &file)
 
+}
+
+// DeleteFileById this method delete file by Id and return answer code and error
+func (us *Uspacy) DeleteFileById(fileId int) (code int, err error) {
+	code, err = us.doDeleteEmptyHeaders(us.buildURL(files.VersionUrl, fmt.Sprintf("%s/%d", files.FilesUrl, fileId)))
+	if err != nil {
+		return code, err
+	}
+	return code, err
+}
+
+// DeleteFilesByEntityId this method delete all files by EntityId and return answer code and error
+func (us *Uspacy) DeleteFilesByEntityId(entityType string, entityId int64) (code int, err error) {
+	code, err = us.doDeleteEmptyHeaders(us.buildURL(files.VersionUrl, fmt.Sprintf("%s?%s&%d", files.FilesUrl, entityType, entityId)))
+	if err != nil {
+		return code, err
+	}
+	return code, err
 }
