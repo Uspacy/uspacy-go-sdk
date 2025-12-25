8930a560cdee5b37c56e8151d8d0dab524df6419
Kyrylo Melnychuk <km@uspacy.com>
2025-12-25 13:57:41 +0000

    Added GetDeals and GetLeads (#122)
    * Added GetDeals and GetLeads
    
    * Review fixes

--- api/crm.go
+++ api/crm.go
@@ -63,9 +63,27 @@ func (us *Uspacy) GetCRMEntitiesForExport(entityType string, params url.Values) 
 	return entities, json.Unmarshal(body, &entities)
 }
 
-// GetEntities this method return arrey of objects and error
+// GetContacts returns an array of contact objects and an error.
 func (us *Uspacy) GetContacts(params url.Values) (entities crm.Contacts, err error) {
 	body, err := us.doGetEmptyHeaders(us.buildURL(crm.VersionUrl, fmt.Sprintf(crm.EntityUrl, crm.ContactsNum.GetUrl())) + "?" + params.Encode())
+	if err != nil {
+		return entities, err
+	}
+	return entities, json.Unmarshal(body, &entities)
+}
+
+// GetDeals returns an array of deal objects and an error.
+func (us *Uspacy) GetDeals(params url.Values) (entities crm.Deals, err error) {
+	body, err := us.doGetEmptyHeaders(us.buildURL(crm.VersionUrl, fmt.Sprintf(crm.EntityUrl, crm.DealsNum.GetUrl())) + "?" + params.Encode())
+	if err != nil {
+		return entities, err
+	}
+	return entities, json.Unmarshal(body, &entities)
+}
+
+// GetLeads returns an array of lead objects and an error.
+func (us *Uspacy) GetLeads(params url.Values) (entities crm.Leads, err error) {
+	body, err := us.doGetEmptyHeaders(us.buildURL(crm.VersionUrl, fmt.Sprintf(crm.EntityUrl, crm.LeadsNum.GetUrl())) + "?" + params.Encode())
 	if err != nil {
 		return entities, err
 	}
